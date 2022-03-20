130cf76708d33cce3f4612632705ba20c15b1262
jerell <jerell1998@gmail.com>
2022-03-20 15:23:58 +0000

    get

---
+++ dist/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import logger from "morgan";
+import path from "path";
+import fs from "fs";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+var __filename = fileURLToPath(import.meta.url);
+var __dirname = dirname(__filename);
+var app = express();
+var port = 3000;
+app.use(logger("dev"));
+app.use(express.static("public"));
+app.get("/", function (req, res) {
+    var publicDir = path.join(__dirname, "../public");
+    var files = fs.readdirSync(publicDir);
+    var now = new Date();
+    var file = files[now.getMinutes() % files.length];
+    res.sendFile(path.join(publicDir, file));
+});
+app.listen(port, function () {
+    console.log("App listening on port ".concat(port));
+});
---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import logger from "morgan";
+import path from "path";
+import fs from "fs";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+const port = 3000;
+
+app.use(logger("dev"));
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  const publicDir = path.join(__dirname, "../public");
+  const files = fs.readdirSync(publicDir);
+
+  const now = new Date();
+  const file = files[now.getMinutes() % files.length];
+
+  res.sendFile(path.join(publicDir, file));
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
